ce469acf6ca24055371827a55e60b971311f18b9
Pedro Lucas <pedrolucasluckow@hotmail.com>
2023-05-26 16:49:09 -0300

    projeto finalizado

---
+++ js/buscarVideo.js
@@ -0,0 +1,27 @@
+import { conectaApi } from "./conectaApi.js"
+import constroiCard from "./mostrarVideos.js"
+
+async function buscarVideo (evento){
+    evento.preventDefault()
+
+    const dadosDaPesquisa = document.querySelector("[data-pesquisa]").value
+    const busca = await conectaApi.buscaVideos(dadosDaPesquisa)
+
+    const lista = document.querySelector("[data-lista]")
+
+    while(lista.firstChild){
+        lista.removeChild(lista.firstChild)
+    }
+
+    busca.forEach(elemento => lista.appendChild(
+        constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)
+    ));
+
+    if(busca.length == 0){
+        lista.innerHTML = `<h2 class="mensagem__titulo">Não existem vídeos com esse termo</h2>`
+    }
+}
+
+const botaoPesquisa = document.querySelector("[data-botao-pesquisa]")
+
+botaoPesquisa.addEventListener("click", evento => buscarVideo(evento))
